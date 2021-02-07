7fa5c265bb109d51f9b82b4f941d6165b620bbec
Adrian Cuevas <adriancuevasdelacruz@gmail.com>
2021-02-07 18:51:27 +0100

    LA PURGA

--- scripts/main.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-# Script donde empezara a llamar a otros scritps para empezar con la instsalacion y configuracion de los programas
-
-# Script para la instalacion de programas
-bash programas.sh
-
-# Configuracion de archivos
-# QTILE
